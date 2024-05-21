0034b7a93f52e5f91357cc86ffe05229fd9a69ec
fonseca-devs <bofonseca.dev@gmail.com>
2024-05-21 19:55:13 -0300

    feat - atualização do Read.me

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,23 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $table = "posts";
+    //caso alguem criou o id de forma não padrão, que é somente id
+    protected $primaryKey = "id";
+    //supondo que alguem criou um id string
+    //protected $keyType = "string";
+    public $incrementing = true;
+    public $timestamps = true;
+
+    const CREATED_AT = "data_criacao";
+    const UPDATED_AT = "data_atualizacao";
+
+    protected $dateFormat = "d/m/Y";
+    protected $connection = 'mysql';
+    protected $attributes = [
+        'title' => 'um comentário'
+    ];
+
     protected $fillable = [
         'user_id',
         'title',
